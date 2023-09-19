316ef4c796f0e026003a3ead30b8432773da6809
HAIDAR123654 <haideralix22104@gmail.com>
2023-09-19 23:53:12 +0530

    clickeventadded

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ const loginPage = document.querySelector(".login");
 loginClose.addEventListener("click", () => {
   loginPage.classList.add("close-login-div");
 });
+
+// event clicked triggred on clicking on anywhere in the DOM-------------------------------
+
+document.addEventListener("click", function (event) {
+  var div = document.getElementById("loginid");
+
+  // Check if the clicked element is the div or one of its descendants
+  if (event.target !== div && !div.contains(event.target)) {
+    // Clicked outside the div, so hide it
+    div.style.display = "none";
+  }
+});
